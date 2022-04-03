7e515b141e0ed7c3cba502544e0f7f7b2f7dc1bc
Abdulkerim Karaman <abdulkerim.karaman@albarakatech.com>
2022-04-03 17:29:45 +0300

    update func

--- index.go
+++ index.go
@@ -4,16 +4,17 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	// Hello world, the web server
+	port := os.Getenv("PORT")
 
 	helloHandler := func(w http.ResponseWriter, req *http.Request) {
 		io.WriteString(w, "Hello, world!\n")
 	}
 
-	http.HandleFunc("/hello", helloHandler)
-	log.Println("Listing for requests at http://localhost:8000/hello")
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	http.HandleFunc("/", helloHandler)
+	log.Println("Listing for" + port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
